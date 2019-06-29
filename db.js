2f346968e7b228b43a9fee505033666e4378b852
Agey <pzyktzle@gmail.com>
2019-06-29 11:17:03 +1000

    Connecting mysql

---
+++ db.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql");
+
+var db_config = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "hotstuff",
+  database: "webapp"
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(err => {
+    if (err) {
+      console.log("error when connecting to DB: " + err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on("error", err => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
