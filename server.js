a288429ab278ddd70b06c400ad31244f762828a1
Agey <pzyktzle@gmail.com>
2019-06-29 10:43:53 +1000

    POST input setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.post("/", (req, res) => {
+  var email = req.body.email;
+  var amount = req.body.amount;
+
+  res.send("*** res: " + email + " " + amount);
+});
+
+app.listen(3000, () => console.log("server is running on port 3000"));
