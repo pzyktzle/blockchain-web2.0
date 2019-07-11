28ae873161fb0cdbbded59a77bdf2f21635287ff
Agey <pzyktzle@gmail.com>
2019-07-11 11:07:09 +1000

    installing paypal SDK

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const publicPath = path.join(__dirname, "./public");
+const paypal = require("paypal-rest-sdk");
 
 const save_user_information = require("./models/server_db");
 
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
+
+// paypal config
+paypal.configure({
+  mode: "sandbox",
+  client_id:
+    "AQFvDKE7ZCnj2uWq-c-Owub_ZVjJ7oo4JT8FbxwkIwnkuVgBltUCH7HpeEWkgRxB3dVtEVDXVd1gjyfb",
+  client_secret:
+    "EBC_W9DLtwrZHaWjr9dR3p84Z-BH3QY6R5ZUPGhlJH-RmTbStwYj5wseJp6G9ReqWo_7CrYjt3nxnbGV"
+});
 
 app.post("/post_info", async (req, res) => {
   var email = req.body.email;
